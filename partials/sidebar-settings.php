57adcadd6e0c3ef71022cc702f0463cf1e9238e1
Abner Perez <abnerstr81@gmail.com>
2022-03-12 16:56:07 -0600

    Sidebar Settings

---
+++ partials/sidebar-settings.php
@@ -0,0 +1,24 @@
+<div class="stg">
+  <div class="stg-top">
+    <span>Settings</span>
+    <i class="fa-solid fa-xmark"></i>
+  </div>
+  <div class="stg-arr">
+    <label for="arrTwoSort">Array to sort</label>
+    <div>
+      <input id="arrTwoSort" type="text" name="arrTwoSort" placeholder="1,7,4,9,20 - Min Length: 2">
+    </div>
+  </div>
+  <div class="stg-mls">
+    <label for="timeTwoSort">Array to sort</label>
+    <div>
+      <input id="timeTwoSort" type="number" name="timeTwoSort" placeholder="Milliseconds">
+    </div>
+  </div>
+  <div class="stg-save">
+    <button id="saveSettings">Save</button>
+  </div>
+  <div class="stg-status">
+    <p id="stgStatus"></p>
+  </div>
+</div>
